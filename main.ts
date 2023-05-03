da932abd0c617f93733a39c3f2bcf9f68ca35b15
Sergiomrcu <132453199+Sergiomrcu@users.noreply.github.com>
2023-05-03 13:19:18 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+function GoBack () {
+	
+}
+function GoPush () {
+	
+}
+function GoDance () {
+	
+}
+let found = false
+let active = true
+while (active) {
+    found = false
+    if (found) {
+        GoPush()
+        GoBack()
+    } else {
+        active = false
+    }
+}
+GoDance()
+basic.forever(function () {
+	
+})
